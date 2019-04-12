5d182e9470e03855eff62ab7a1629dbd851e6e98
Johan Wänglöf <jwanglof@gmail.com>
2019-04-12 19:57:26 +0200

    Moves, etc

--- src/components/SetsReps/SetsRepsService.ts
+++ src/components/SetsReps/SetsRepsService.ts
@@ -1,8 +1,14 @@
 import firebase from '../../config/firebase';
 import {isEmpty} from 'lodash';
 import {ISetBasicModel, ISetModel, ISetModelWithoutUid} from '../../models/ISetModel';
-import {FirebaseCollectionNames, getNowTimestamp, getSetErrorObject} from '../../config/FirebaseUtils';
+import {
+  FirebaseCollectionNames,
+  getNowTimestamp,
+  getSetErrorObject,
+  getSetsRepsExerciseErrorObject
+} from '../../config/FirebaseUtils';
 import {Versions} from '../../models/IBaseModel';
+import {ISetsRepsModel, ISetsRepsModelWithoutUid} from '../../models/ISetsRepsModel';
 
 export const deleteSet = async (setUid: string): Promise<void> => {
   return await firebase.firestore()
@@ -54,4 +60,43 @@ export const addSetToSetsRepsExerciseArray = async (setUid: string, exerciseUid:
     .collection(FirebaseCollectionNames.FIRESTORE_COLLECTION_EXERCISE_TYPE_SETS_REPS)
     .doc(exerciseUid)
     .update({sets: firebase.firestore.FieldValue.arrayUnion(setUid)});
+};
+
+export const addNewSetsRepsExerciseAndGetUid = async (ownerUid: string): Promise<string> => {
+  const setsRepsData: ISetsRepsModelWithoutUid = {
+    sets: [],
+    ownerUid,
+    createdTimestamp: getNowTimestamp(),
+    version: Versions.v1
+  };
+  const exerciseSetsRepsDocRef = await firebase.firestore()
+    .collection(FirebaseCollectionNames.FIRESTORE_COLLECTION_EXERCISE_TYPE_SETS_REPS)
+    .add(setsRepsData);
+  return exerciseSetsRepsDocRef.id;
+};
+
+export const getSetsRepsExercise = async (exerciseUid: string): Promise<ISetsRepsModel> => {
+  const querySnapshot = await firebase.firestore()
+    .collection(FirebaseCollectionNames.FIRESTORE_COLLECTION_EXERCISE_TYPE_SETS_REPS)
+    .doc(exerciseUid)
+    .get();
+  if (querySnapshot.exists && !isEmpty(querySnapshot.data())) {
+    const exerciseData = querySnapshot.data()!;
+    return {
+      sets: exerciseData.sets,
+      uid: querySnapshot.id,
+      ownerUid: exerciseData.ownerUid,
+      createdTimestamp: exerciseData.createdTimestamp,
+      version: exerciseData.version
+    };
+  } else {
+    throw getSetsRepsExerciseErrorObject(exerciseUid);
+  }
+};
+
+export const deleteSetsRepsExercise = async (exerciseUid: string): Promise<void> => {
+  return await firebase.firestore()
+    .collection(FirebaseCollectionNames.FIRESTORE_COLLECTION_EXERCISE_TYPE_SETS_REPS)
+    .doc(exerciseUid)
+    .delete();
 };
