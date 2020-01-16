4fea2303e321f4574cc339b0c47d5f02e066a185
Johan Wänglöf <jwanglof@gmail.com>
2020-01-16 19:59:55 +0100

    Starting to get rid of `react-formik-ui`
    The components can be re-made with pure formik instead

--- src/components/Formik/FieldFormGroup.tsx
+++ src/components/Formik/FieldFormGroup.tsx
@@ -4,14 +4,15 @@ import {colSmSize, labelSmSize} from '../../utils/formik-utils';
 import {ErrorMessage, Field} from 'formik';
 import {IFormikProps} from '../../utils/ts-formik-utils';
 
-const FieldFormGroup: FunctionComponent<IFormikProps> = ({labelText, name, type="text", labelHidden=false, inputProps}) => {
-  return (<FormGroup row>
+const FieldFormGroup: FunctionComponent<IFormikProps> = ({labelText, name, type="text", labelHidden=false, inputProps}) => (
+  <FormGroup row>
     {!labelHidden && <Label for={name} sm={labelSmSize}>{labelText}</Label>}
     <Col sm={colSmSize}>
-      <Input tag={Field} type={type} component="input" name={name} id={name} placeholder={labelText} {...inputProps} />
+      <Input tag={Field} type={type} component="input" name={name} id={name} className="form-control" placeholder={labelText} {...inputProps} />
       <ErrorMessage name={name}>{msg => <Alert color="warning" className="pb-0 pt-0 pl-2 pr-2 mt-2">{msg}</Alert>}</ErrorMessage>
     </Col>
-  </FormGroup>);
-};
+  </FormGroup>
+);
+
 
 export default FieldFormGroup;
--- src/components/Formik/SelectFormGroup.tsx
+++ src/components/Formik/SelectFormGroup.tsx
@@ -2,24 +2,28 @@ import React, {FunctionComponent} from 'react';
 import {IFormikProps} from '../../utils/ts-formik-utils';
 import {Alert, Col, FormGroup, Label} from 'reactstrap';
 import {colSmSize, labelSmSize} from '../../utils/formik-utils';
-import {ErrorMessage} from 'formik';
-// @ts-ignore
-import {Select} from 'react-formik-ui';
+import {ErrorMessage, Field} from 'formik';
 
-const SelectFormGroup: FunctionComponent<ISelectFormGroup> = ({labelText, name, options, labelHidden=false, ...inputProps}) => {
-  return (
-    <FormGroup row>
-      {!labelHidden && <Label for={name} sm={labelSmSize}>{labelText}</Label>}
-      <Col sm={colSmSize}>
-        <Select name={name} options={options} {...inputProps}/>
-        <ErrorMessage name={name}>{msg => <Alert color="warning" className="pb-0 pt-0 pl-2 pr-2 mt-2">{msg}</Alert>}</ErrorMessage>
-      </Col>
-    </FormGroup>
-  );
-};
+const SelectFormGroup: FunctionComponent<ISelectFormGroup> = ({labelText, name, options, labelHidden=false}) => (
+  <FormGroup row>
+    {!labelHidden && <Label for={name} sm={labelSmSize}>{labelText}</Label>}
+    <Col sm={colSmSize}>
+      <Field as="select" name={name} id={name} className="form-control">
+        {options.map((option, i) => (<option key={i} value={option.value} disabled={option.disabled}>{option.label}</option>))}
+      </Field>
+      <ErrorMessage name={name}>{msg => <Alert color="warning" className="pb-0 pt-0 pl-2 pr-2 mt-2">{msg}</Alert>}</ErrorMessage>
+    </Col>
+  </FormGroup>
+);
 
 interface ISelectFormGroup extends IFormikProps {
-  options: any
+  options: ISelectFormOptions[]
+}
+
+export interface ISelectFormOptions {
+  value: string
+  label: string
+  disabled?: boolean
 }
 
 export default SelectFormGroup;
