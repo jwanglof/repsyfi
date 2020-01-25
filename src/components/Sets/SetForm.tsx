12a12d6307ba0e2e37b3e52d62fad984eb6d59c5
Johan Wänglöf <jwanglof@gmail.com>
2020-01-25 18:07:24 +0100

    Closes #47

--- src/components/Sets/SetForm.tsx
+++ src/components/Sets/SetForm.tsx
@@ -1,5 +1,5 @@
 import React, {FunctionComponent} from 'react';
-import {Formik, FormikHelpers, Form} from 'formik';
+import {Form, Formik, FormikHelpers} from 'formik';
 import {ISetModel} from '../../models/ISetModel';
 import FormikField from '../Formik/FormikField';
 import {useTranslation} from 'react-i18next';
@@ -7,7 +7,6 @@ import {Button, ButtonGroup, Col, Row} from 'reactstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {setsValidation} from './SetsHelpers';
 import {ExerciseTypesEnum} from '../../enums/ExerciseTypesEnum';
-import {InputType} from 'reactstrap/lib/Input';
 
 const SetForm: FunctionComponent<ISetFormProps> = ({hideFormCb, exerciseType, currentData, onSubmit, extraButtonGroups}) => {
   const { t } = useTranslation();
@@ -18,46 +17,33 @@ const SetForm: FunctionComponent<ISetFormProps> = ({hideFormCb, exerciseType, cu
     validate={(values: any) => {
       return setsValidation(values, t);
     }}>
-    {(props) => {
-      const {errors, isSubmitting, values} = props;
-      let amountInKgString = values.amountInKg.toString();
-      console.log(values, parseInt(amountInKgString), parseFloat(amountInKgString), parseFloat(amountInKgString) === parseInt(amountInKgString));
-      let amountInKgType: InputType = 'number';
-      let amountInKgInputProps = {min: 0, autoFocus: true};
-      if (parseFloat(amountInKgString) !== parseInt(amountInKgString)) {
-        amountInKgType = 'text';
-        // delete amountInKgInputProps.min;
-        // delete amountInKgInputProps.autoFocus;
-      }
-      console.log('type::', amountInKgType, amountInKgInputProps);
-      return (
-        <>
-          {isSubmitting && <Row><Col className="text-center"><FontAwesomeIcon icon="spinner" spin/></Col></Row>}
-          {!isSubmitting && <Form>
-            <div className="row pt-1 pb-1">
-              <Col className="pt-1" xs={2}>{currentData.index}</Col>
-              <Col xs={5}>
-                <FormikField name="amountInKg" labelText={t('Amount in KG')} type={amountInKgType} labelHidden
-                             inputProps={amountInKgInputProps}/>
-              </Col>
-              {exerciseType === ExerciseTypesEnum.EXERCISE_TYPE_SETS_REPS &&
-              <Col xs={5}><FormikField name="reps" labelText={t('Repetitions')} type="number" labelHidden
-                                       inputProps={{min: 0}}/></Col>}
-              {exerciseType === ExerciseTypesEnum.EXERCISE_TYPE_SETS_SECONDS &&
-              <Col xs={5}><FormikField name="seconds" labelText={t('Seconds')} type="number" labelHidden
-                                       inputProps={{min: 0}}/></Col>}
-            </div>
-            <Row>
-              <ButtonGroup className="w-100" vertical>
-                <Button type="submit" color="primary" disabled={isSubmitting || !errors}>{t('Save set')}</Button>
-                <Button color="danger" onClick={() => hideFormCb()}>{t('Discard set')}</Button>
-                {extraButtonGroups}
-              </ButtonGroup>
-            </Row>
-          </Form>}
-        </>
-      );
-    }}
+    {({errors, isSubmitting}) => (
+      <>
+        {isSubmitting && <Row><Col className="text-center"><FontAwesomeIcon icon="spinner" spin/></Col></Row>}
+        {!isSubmitting && <Form>
+          <div className="row pt-1 pb-1">
+            <Col className="pt-1" xs={2}>{currentData.index}</Col>
+            <Col xs={5}>
+              <FormikField name="amountInKg" labelText={t('Amount in KG')} type="number" labelHidden
+                           inputProps={{min: 0, autoFocus: true, step: '0.1'}}/>
+            </Col>
+            {exerciseType === ExerciseTypesEnum.EXERCISE_TYPE_SETS_REPS &&
+            <Col xs={5}><FormikField name="reps" labelText={t('Repetitions')} type="number" labelHidden
+                                     inputProps={{min: 0}}/></Col>}
+            {exerciseType === ExerciseTypesEnum.EXERCISE_TYPE_SETS_SECONDS &&
+            <Col xs={5}><FormikField name="seconds" labelText={t('Seconds')} type="number" labelHidden
+                                     inputProps={{min: 0}}/></Col>}
+          </div>
+          <Row>
+            <ButtonGroup className="w-100" vertical>
+              <Button type="submit" color="primary" disabled={isSubmitting || !errors}>{t('Save set')}</Button>
+              <Button color="danger" onClick={() => hideFormCb()}>{t('Discard set')}</Button>
+              {extraButtonGroups}
+            </ButtonGroup>
+          </Row>
+        </Form>}
+      </>
+    )}
   </Formik>;
 };
 
